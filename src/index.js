d0ce95b67dda480a7db35f1ad711181a749d3363
Gustavo Hideki <gustavohideki@outlook.com>
2022-06-26 00:57:31 -0300

    Finaliza remoção automática de usuários inativos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 
 dotenv.config();
 
@@ -30,7 +31,8 @@ server.post("/participants", async (req, res) => {
         res.sendStatus(409);
         return;
     } else {
-        db.collection("participantes").insertOne({ name, lastStatus: Date.now() });
+        await db.collection("participantes").insertOne({ name, lastStatus: Date.now() });
+        await db.collection("mensagens").insertOne({ from: name, to: "Todos", text: "entra na sala...", type: "status", time: dayjs().format("HH:mm:ss") });
         res.sendStatus(201);
     }
 });
@@ -39,5 +41,15 @@ server.get("/participants", async(req, res) => {
     const allParticipants = await db.collection("participantes").find({}).toArray();
     res.send(allParticipants);
 })
+
+// Remove usuários inativos
+setInterval(async() => {
+    const now = Date.now() - 10000;
+    const users = await db.collection("participantes").find({ lastStatus: { $lt: now} }).toArray();
+    users.forEach(async(element) => {
+        await db.collection("mensagens").insertOne({ from: element.name, to: "Todos", text: "sai da sala...", type: "status", time: dayjs().format("HH:mm:ss") });
+    });
+    await db.collection("participantes").deleteMany({ lastStatus: { $lt: now} });
+},15000)
 
 server.listen(5000, () => {console.log("Servidor rodando!")});
