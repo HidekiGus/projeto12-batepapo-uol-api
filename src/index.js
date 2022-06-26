f97883b955ff891ea894910eadb3da05ee10e570
Gustavo Hideki <gustavohideki@outlook.com>
2022-06-26 07:56:50 -0300

    Finaliza post messages sem joi

--- src/index.js
+++ src/index.js
@@ -44,7 +44,11 @@ server.get("/participants", async(req, res) => {
 server.post("/messages", async(req, res) => {
     const { to, text, type } = req.body;
     const user = req.headers.user;
-})
+    await db.collection("mensagens").insertOne({ from: user, to, text, type, time: dayjs().format("HH:mm:ss")});
+    res.sendStatus(201);
+});
+
+
 
 // Remove usuários inativos
 setInterval(async() => {
@@ -55,6 +59,6 @@ setInterval(async() => {
     });
     await db.collection("participantes").deleteMany({ lastStatus: { $lt: now} });
     console.log("Usuários inativos removidos");
-},15000)
+},15000);
 
 server.listen(5000, () => {console.log("Servidor rodando!")});
