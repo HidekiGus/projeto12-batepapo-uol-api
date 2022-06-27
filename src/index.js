17dc62e3d716fb935d4256ece2434131df43efab
Gustavo Hideki <gustavohideki@outlook.com>
2022-06-27 00:17:27 -0300

    Finaliza POST messages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import joi from "joi";
 
 dotenv.config();
 
-const cliente = new MongoClient("mongodb://127.0.0.1:27017");
+const cliente = new MongoClient(URL_CONNECT_MONGO);
 let db;
 
 cliente.connect().then(() => {
@@ -46,6 +46,11 @@ server.get("/participants", async(req, res) => {
 })
 
 server.post("/messages", async(req, res) => {
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: 
+    })
     const { to, text, type } = req.body;
     const user = req.headers.user;
     await db.collection("mensagens").insertOne({ from: user, to, text, type, time: dayjs().format("HH:mm:ss")});
